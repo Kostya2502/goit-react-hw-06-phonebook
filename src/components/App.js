4bfc82540b1fbfdba7f591d7f074a4cf3fc88586
Kostya Panasiuk <kos2502@ukr.net>
2021-02-02 19:01:34 +0200

    add redux toolkit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ContactForm from "./contactForm/ContactForm";
+import ContactList from "./contactList/ContactList";
+import Filter from "./filter/Filter";
+import { CSSTransition } from 'react-transition-group';
+import style from './App.module.css';
+import { connect } from "react-redux";
 
-class App extends Component {
-    state = {}
-    render() {
-        return (
-            <>
-                <h1>HELLO</h1>
-            </>
-        );
-    }
+const App = ({ value, contacts }) => {
+    return (
+        <>
+            <CSSTransition in={true} classNames={style} appear={true} timeout={1000} unmountOnExit>
+                <h1>Phonebook</h1>
+            </CSSTransition>
+            <ContactForm />
+            <h2>Contacts</h2>
+            {(value.length > 0 || contacts.length > 1) && <Filter />}
+            <ContactList />
+        </>
+    );
 }
 
-export default (App);
+const mapStateToProps = (state) => ({
+    value: state.contacts.filter,
+    contacts: state.contacts.items,
+})
+
+export default connect(mapStateToProps)(App);
